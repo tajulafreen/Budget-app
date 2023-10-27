820d9df56d28f49ddedcbea4f209d5af983095a2
afreen shaik <affushaik9493@gmail.com>
2023-10-27 15:56:14 +0530

    add views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord 
-  has_many :categories
-  has_many :records
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :categories, dependent: :destroy
+  has_many :records, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, :email, presence: true
 end
